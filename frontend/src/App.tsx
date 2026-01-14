87825fdbcd62b328dc8b78c2882924e02c26cbfd
Brian <briansalise1@gmail.com>
2026-01-14 21:26:02 +0800

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { BrowserRouter,Route,Routes} from 'react-router-dom'
+import MainPage from './pages/MainPage'
+import AllProjects from './components/AllProjects'
+function App() {
+
+
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<MainPage/>}/>
+        <Route path='/projects' element={<AllProjects darkMode={false}/>}/>
+      </Routes>
+    </BrowserRouter>
+
+    </>
+  )
+}
+
+export default App
